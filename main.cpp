b5cc1f0acdc1bfb2619c974cfd89669d413c5032
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-07 12:43:18 +0300

    define constants

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 
+const string BASE_DIR = "C:\\Users\\Admin\\AppData\\Local\\SumatraPDF";
+const string FILE_NAME = "SumatraPDF-settings.txt";
+const string DARK = "TextColor = #eeeeee\nBackgroundColor = #111111";
+const string LIGHT = "TextColor = #000000\nBackgroundColor = #ffffff  ";
+
 int main() {
     cout<<"Hello, world";
     return 0;
