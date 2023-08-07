a687ff34c84e352ad463ff7c281391ce18e807fd
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-07 12:47:12 +0300

    read the theme from the txt file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
 using namespace std;
 
 const string BASE_DIR = "C:\\Users\\Admin\\AppData\\Local\\SumatraPDF";
@@ -6,7 +9,30 @@ const string FILE_NAME = "SumatraPDF-settings.txt";
 const string DARK = "TextColor = #eeeeee\nBackgroundColor = #111111";
 const string LIGHT = "TextColor = #000000\nBackgroundColor = #ffffff  ";
 
-int main() {
-    cout<<"Hello, world";
+int main()
+{
+    fstream file;
+    string ind_file_data;
+    vector<string> file_data;
+    // file.open(BASE_DIR + "\\" + FILE_NAME);
+
+    file.open("SumatraPDF-settings.txt");
+
+    if (file.fail())
+    {
+        cout << "Error opening the file";
+    }
+
+    int i = 1;
+    while (!file.eof())
+    {
+        if (i > 12)
+        {
+            break;
+        }
+        getline(file, ind_file_data);
+        file_data.push_back(ind_file_data);
+        i++;
+    }
     return 0;
 }
