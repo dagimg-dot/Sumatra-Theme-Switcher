e3ea69f442be96c632f60134ff8cbcac40facce4
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-07 12:50:16 +0300

    add function to get current theme

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ const string FILE_NAME = "SumatraPDF-settings.txt";
 const string DARK = "TextColor = #eeeeee\nBackgroundColor = #111111";
 const string LIGHT = "TextColor = #000000\nBackgroundColor = #ffffff  ";
 
+Theme getCurrentTheme(string line);
+
 int main()
 {
     fstream file;
@@ -39,5 +41,17 @@ int main()
         file_data.push_back(ind_file_data);
         i++;
     }
+
+    Theme currentTheme = getCurrentTheme(file_data[10]);
+
     return 0;
+}
+
+Theme getCurrentTheme(string line)
+{
+    if (line.substr(12) == "#eeeeee" || line.substr(13) == "#eeeeee")
+    {
+        return Theme::Dark;
+    }
+    return Theme::Light;
 }
